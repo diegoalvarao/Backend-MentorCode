bbd76a21c3f27cd7ace28b19c219718768f4fb85
diegoalvarao <diegoalejoalvarado@gmail.com>
2023-10-27 17:07:44 -0500

    Carga del proyecto raiz

---
+++ MentorCode/src/main/java/com/example/MentorCode/MentorCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.MentorCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorCodeApplication.class, args);
+	}
+
+}
---
+++ MentorCode/src/test/java/com/example/MentorCode/MentorCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MentorCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
