a61725b54c555f23b0696647bd55bcd020fe9b8c
diegoalvarao <diegoalejoalvarado@gmail.com>
2023-11-08 11:33:06 -0500

    Funcionamiento de Login y Registro

--- MentorCode/src/main/java/com/example/MentorCode/MentorCodeApplication.java
+++ MentorCode/src/main/java/com/example/MentorCode/MentorCodeApplication.java
@@ -2,6 +2,8 @@ package com.example.MentorCode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MentorCodeApplication {
@@ -9,6 +11,15 @@ public class MentorCodeApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(MentorCodeApplication.class, args);
+	}
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
+			}
+		};
 	}
 
 
